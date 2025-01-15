8822692bc3d8e5dc70a6a99289518163b850e91d
akshay-sri <sriakshay2611999@gmail.com>
2025-01-16 01:18:28 +0530

    Solution 3 edited

--- Java8-PracticeQuestions/Q3_AvgOfAllNumbers.java
+++ Java8-PracticeQuestions/Q3_AvgOfAllNumbers.java
@@ -2,13 +2,8 @@ import java.util.*;
 public class Q3_AvgOfAllNumbers {
     public static void main(String[] args) {
         Integer[] arr = {1,2,3,4,5,6};
-        List<Integer> list = Arrays.asList(arr);
         //average cannot be applied directly on stream so first we need to convert to int(mapToInt())
-        double ans = list
-        .stream()
-        .mapToInt(e->e)
-        .average()
-        .getAsDouble();
-        System.out.println(ans);
+        double a = Arrays.stream(arr).mapToInt(x->x).average().getAsDouble();
+        System.out.println(a);
     }
 }
